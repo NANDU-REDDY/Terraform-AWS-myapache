31f48c5b152fa96da04a515def7d505c6019c081
NANDU-REDDY <nandureddy7036@gmail.com>
2021-10-11 08:08:39 +0530

    My terraform

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-e214659f"
+}
+
+
+
+
